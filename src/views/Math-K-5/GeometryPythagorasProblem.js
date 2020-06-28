e6a19c385e8267888029ae1a59cf0636b72cdaf6
Sanjay Noronha <snoronha@gmail.com>
2020-06-27 18:23:06 -0700

    Intermediate commit. Shows angles arcs (not values) and lengths of triangle sides (horizontal)

--- src/views/Math-K-5/GeometryPythagorasProblem.js
+++ src/views/Math-K-5/GeometryPythagorasProblem.js
@@ -46,33 +46,88 @@ export default function GeometryPythagorasProblem(props) {
     setSoln(tmpSoln);
   };
 
+  // Sides a, b, c (c is base), angles A, B, C in degrees
+  // Input expected: b, c, A (two sides + included angle)
+  const TriangleSidesAngle = (props) => {
+    const r = 20;
+    const A = props.A; // A in degrees
+    const Arad = (A * Math.PI) / 180;
+    const b = props.b; // length of side b
+    const c = props.c; // length of side c
+    const cx = b * Math.cos(Arad);
+    const cy = b * Math.sin(Arad);
+    const pointA = `0,${cy}`;
+    const pointB = `${c},${cy}`;
+    const pointC = `${cx},0`;
+    const points = `${pointA} ${pointB} ${pointC}`;
+    const a = Math.sqrt(b * b + c * c - 2 * b * c * Math.cos(Arad));
+    const Brad = Math.asin((Math.sin(Arad) * b) / a);
+    const Crad = Math.PI - (Arad + Brad);
+    const pathA = `M ${r} 0 A ${r} ${r} 0 0 1 ${Math.cos(Arad) * r} ${
+      Math.sin(Arad) * r
+    }`;
+    const pathB = `M ${r} 0 A ${r} ${r} 0 0 1 ${Math.cos(Brad) * r} ${
+      Math.sin(Brad) * r
+    }`;
+    const pathC = `M ${r} 0 A ${r} ${r} 0 0 1 ${Math.cos(Crad) * r} ${
+      Math.sin(Crad) * r
+    }`;
+    const transformA = `translate(0,${cy}) rotate(${-A})`;
+    const transformB = `translate(${props.c},${cy}) rotate(180)`;
+    const transformC = `translate(${cx},0) rotate(${(Brad * 180) / Math.PI})`;
+    return (
+      <g stroke={"#888"} strokeWidth={1}>
+        <polygon points={points} fill={"#ffa"} opacity={0.8} />
+        <path fill={"none"} d={pathA} transform={transformA} />
+        <path fill={"none"} d={pathB} transform={transformB} />
+        <path fill={"none"} d={pathC} transform={transformC} />
+        <text x={(c + cx) / 2} y={cy / 2}>
+          {Math.floor(a)}
+        </text>
+        <text x={c / 2} y={cy}>
+          {c}
+        </text>
+        <text x={cx / 2} y={cy / 2}>
+          {b}
+        </text>
+      </g>
+    );
+  };
+
+  const Triangle = (props) => {
+    return (
+      <polygon
+        points={props.points}
+        fill={"#ffa"}
+        stroke={"#888"}
+        strokeWidth={1}
+      />
+    );
+  };
+
+  const Circle = (props) => {
+    return (
+      <circle
+        cx={props.cx}
+        cy={props.cy}
+        r={props.r}
+        fill={"#fdd"}
+        opacity={0.3}
+        stroke={"#a00"}
+        strokeWidth={1}
+      />
+    );
+  };
+
   return (
     <div style={props.style}>
-      <svg height={150} width={150}>
-        <polygon
-          points={"20,130 130,130 20,0"}
-          fill={"#ffa"}
-          stroke={"#888"}
-          strokeWidth={1}
-        />
-        <circle
-          cx={60}
-          cy={60}
-          r={50}
-          fill={"#fcc"}
-          opacity={0.3}
-          stroke={"#a00"}
-          strokeWidth={1}
-        />
-        <text x={10} y={65}>
-          4
-        </text>
-        <text x={65} y={145}>
-          3
-        </text>
-        <text x={0} y={0} transform={"translate(80, 65) rotate(50)"}>
-          c = ?
-        </text>
+      <svg height={150} width={250}>
+        <g transform={"translate(25, 25) rotate(0)"}>
+          <TriangleSidesAngle b={140} c={180} A={50} />
+          <text x={0} y={0} transform={"translate(80, 65) rotate(50)"}>
+            c
+          </text>
+        </g>
       </svg>
     </div>
   );
