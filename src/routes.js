999fb35ab4c779afa4a11b380da91b8c8ef61b38
Sanjay Noronha <snoronha@gmail.com>
2020-10-05 21:39:42 -0700

    Cleanup

--- src/routes.js
+++ src/routes.js
@@ -9,14 +9,6 @@ import {
   faHandsHelping,
   faUserEdit,
 } from "@fortawesome/free-solid-svg-icons";
-// core components/views for Admin layout
-// import DashboardPage from "views/Dashboard/Dashboard.js";
-// import UserProfile from "views/UserProfile/UserProfile.js";
-// import TableList from "views/TableList/TableList.js";
-// import Typography from "views/Typography/Typography.js";
-// import Icons from "views/Icons/Icons.js";
-// import Maps from "views/Maps/Maps.js";
-// import NotificationsPage from "views/Notifications/Notifications.js";
 import MathK5Page from "views/Math-K-5/MathK5.js";
 import MyPractice from "views/MyPractice/MyPractice.js";
 import SolveIt from "views/SolveIt/SolveIt.js";
@@ -81,47 +73,6 @@ const dashboardRoutes = [
     component: AboutPage,
     layout: "/admin",
   },
-  /*
-  {
-    path: "/table",
-    name: "Table List",
-    icon: "content_paste",
-    component: TableList,
-    layout: "/admin",
-  },
-
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: LibraryBooks,
-    component: Typography,
-    layout: "/admin",
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    rtlName: "الرموز",
-    icon: BubbleChart,
-    component: Icons,
-    layout: "/admin",
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    rtlName: "خرائط",
-    icon: LocationOn,
-    component: Maps,
-    layout: "/admin",
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    rtlName: "إخطارات",
-    icon: Notifications,
-    component: NotificationsPage,
-    layout: "/admin",
-  },
-  */
 ];
 
 export default dashboardRoutes;
